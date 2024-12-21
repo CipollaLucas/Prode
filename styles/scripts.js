94237f71e0956b34fc9d3c350d13e1b4626f4c3e
Lucas Cipolla <lucascipolla1993@gmail.com>
2024-12-21 17:22:34 -0300

    login front

---
+++ styles/scripts.js
@@ -0,0 +1,5 @@
+// Obtener el botón por su ID
+document.getElementById("loginBtn").addEventListener("click", function() {
+    // Redirigir a login.html al hacer clic en el botón
+    window.location.href = "login.html";
+    }); 
